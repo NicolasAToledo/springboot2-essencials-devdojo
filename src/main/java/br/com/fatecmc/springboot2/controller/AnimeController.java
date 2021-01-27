943775ef24ed9bf87cea217d6975feb797a95290
Nicolas Toledo <nicolas.toledo@fatec.sp.gov.br>
2021-01-27 02:42:09 -0300

    Metodo POST

--- src/main/java/br/com/fatecmc/springboot2/controller/AnimeController.java
+++ src/main/java/br/com/fatecmc/springboot2/controller/AnimeController.java
@@ -9,11 +9,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 //import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -44,6 +41,12 @@ public class AnimeController {
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
         return ResponseEntity.ok(animeService.findById(id));
         //OU return ResponseEntity.ok<>(animeService.listAll());
+    }
+
+    @PostMapping
+    //@ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
     }
 
 }
--- src/main/java/br/com/fatecmc/springboot2/service/AnimeService.java
+++ src/main/java/br/com/fatecmc/springboot2/service/AnimeService.java
@@ -6,13 +6,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
 
 //É como se fosse a classe responsavel pela regra de negocio (RN)
 @Service
 public class AnimeService  {
 
-    private List<Anime> animes = List.of(new Anime( 1L,"Nanatsu"), new Anime(2L,"Naruto"));
+    private static List<Anime> animes;
+    static{
+        animes = new ArrayList<>(List.of(new Anime( 1L,"Nanatsu"), new Anime(2L,"Naruto")));
+    }
     //implements AnimeRepository
     //private final AnimeRepository animeRepository;
     public List<Anime> listAll(){
@@ -24,5 +29,11 @@ public class AnimeService  {
                 .filter(anime -> anime.getId().equals(id))
                 .findFirst()
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
+    }
+
+    public Anime save(Anime anime){
+        anime.setId(ThreadLocalRandom.current().nextLong(3, 100000));
+        animes.add(anime);
+        return anime;
     }
 }
