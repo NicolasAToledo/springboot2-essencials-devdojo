eb8a8010e9bd33c414737f920813c37427e32e2d
Nicolas Toledo <nicolas.toledo@fatec.sp.gov.br>
2021-01-27 02:11:30 -0300

    Metodo GET part 2

--- src/main/java/br/com/fatecmc/springboot2/controller/AnimeController.java
+++ src/main/java/br/com/fatecmc/springboot2/controller/AnimeController.java
@@ -7,7 +7,10 @@ import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 //import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,8 +33,17 @@ public class AnimeController {
     //@RequestMapping(method = RequestMethod.GET, path = "list")JEITO_ANTIGO
 
     @GetMapping
-    public List<Anime> list(){
+    public ResponseEntity<List<Anime>> list(){
         log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
-        return animeService.listAll();
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+        //OU return ResponseEntity.ok<>(animeService.listAll());
     }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        log.info(dateUtil.formatLocalDateTimeToDatabaseStyle(LocalDateTime.now()));
+        return ResponseEntity.ok(animeService.findById(id));
+        //OU return ResponseEntity.ok<>(animeService.listAll());
+    }
+
 }
--- src/main/java/br/com/fatecmc/springboot2/service/AnimeService.java
+++ src/main/java/br/com/fatecmc/springboot2/service/AnimeService.java
@@ -2,16 +2,27 @@ package br.com.fatecmc.springboot2.service;
 
 import br.com.fatecmc.springboot2.domain.Anime;
 import br.com.fatecmc.springboot2.repository.AnimeRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 //É como se fosse a classe responsavel pela regra de negocio (RN)
 @Service
 public class AnimeService  {
+
+    private List<Anime> animes = List.of(new Anime( 1L,"Nanatsu"), new Anime(2L,"Naruto"));
     //implements AnimeRepository
     //private final AnimeRepository animeRepository;
     public List<Anime> listAll(){
-        return List.of(new Anime( 1L,"Nanatsu"), new Anime(2L,"Naruto"));
+        return animes;
+    }
+
+    public Anime findById(long id){
+        return animes.stream()
+                .filter(anime -> anime.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Anime not Found"));
     }
 }
